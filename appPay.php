e8c3989830a33aad36d49e7fb574337d9949f9a5
ipipip1735 <toyota0626>
2019-08-07 06:07:57 +0800

    add configuration

---
+++ appPay.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/8/4
+ * Time: 23:12
+ */
+
+include_once "AopSdk.php";
+include_once "config.php";
+
+$aop = new AopClient;
+$aop->gatewayUrl = $config['gatewayUrl'];
+$aop->appId = $config['appId'];
+$aop->rsaPrivateKey = $config['rsaPrivateKey'];
+$aop->format = $config['format'];
+$aop->postCharset = $config['postCharset'];
+$aop->signType = $config['signType'];
+$aop->alipayrsaPublicKey = $config['alipayrsaPublicKey'];
+
+
+$request = new AlipayTradeAppPayRequest();
+$request->setNotifyUrl($config['notify_url']);
+//$request->setReturnUrl($config['return_url']);
+
+
+$bizcontent = array(
+    'out_trade_no' => time(),
+    'subject' => 'XXXX',
+    'total_amount' => 10.10);
+$bizcontent = json_encode($bizcontent);
+//echo $bizcontent;
+
+
+$request->setBizContent($bizcontent);
+$result = $aop->pageExecute($request);
+echo $result;
